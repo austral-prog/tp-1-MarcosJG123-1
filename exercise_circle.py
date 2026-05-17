4bedca2c904abd431d17f910ba57b85225c325df
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-17 00:15:08 -0300

    Update exercise_circle.py

--- exercise_circle.py
+++ exercise_circle.py
@@ -4,7 +4,6 @@ from math import pi
 def circle():
     """
     Ejercicio 6 - Geometría de Círculo
-
     Dado el radio de un círculo, calcular e imprimir:
     1. El área (π × radio²)
     2. La circunferencia (2 × π × radio)
